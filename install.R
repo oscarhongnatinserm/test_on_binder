4fa34a271291039c6bf35eefe521c44800c15813
oscar hongnat <ohongnat@broussais.inserm.fr>
2021-09-14 17:11:35 +0200

    test

---
+++ install.R
@@ -0,0 +1,12 @@
+install.packages('tidyverse')
+install.packages('rlist')
+install.packages('data.table')
+install.packages('dplyr')
+install.packages('XML')
+install.packages('methods')
+
+# We fill the folder where are the files that we need to open
+folder = "C:/Users/ohongnat/Documents/Stats_nomenclature_jupyter"
+
+# We set the folder
+setwd(folder)
