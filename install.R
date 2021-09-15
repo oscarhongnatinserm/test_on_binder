58a283c34e6bba88ef919dbebe1fae12f7853ef1
oscar hongnat <ohongnat@broussais.inserm.fr>
2021-09-15 10:01:37 +0200

    update install.R

--- install.R
+++ install.R
@@ -5,8 +5,8 @@ install.packages('dplyr')
 install.packages('XML')
 install.packages('methods')
 
-# We fill the folder where are the files that we need to open
-folder = "C:/Users/ohongnat/Documents/Stats_nomenclature_jupyter"
-
-# We set the folder
-setwd(folder)
+# # We fill the folder where are the files that we need to open
+# folder = "C:/Users/ohongnat/Documents/Stats_nomenclature_jupyter"
+# 
+# # We set the folder
+# setwd(folder)
